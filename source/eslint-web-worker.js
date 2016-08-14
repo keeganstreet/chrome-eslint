f3bc03efb03e1958859e5e437d3f7544447778ae
Keegan Street <kestreet@deloitte.com.au>
2016-08-14 16:19:34 +1000

    Moved linting process into a web worker because it takes quite a while, and it was locking up the UI

---
+++ source/eslint-web-worker.js
@@ -0,0 +1,53 @@
+var lint = function(data) {
+	// Escodegen must be loaded before require.js.
+	// It has been built with "CommonJS Everywhere" and will not execute properly if require is defined.
+	importScripts(data.url + '/libs/escodegen.browser.js');
+
+	// Require.js is required to load ESLint. ESLint has been built with Browserify.
+	importScripts(data.url + '/libs/require.js');
+
+	require.config({
+		baseUrl: data.url
+	});
+
+	require(['/libs/eslint.js'], function(ESLint) {
+		// Espree is also included in eslint.js
+		var espree = require('espree');
+
+		// It is hard to read the linting output for a minified file,
+		// so generate an Abstract Syntax Tree (AST), then convert
+		// the AST into a nicely formatted script with one operation per line.
+		var ast = espree.parse(data.code);
+		var codeBeautified = escodegen.generate(ast);
+
+		var messages = ESLint.verify(codeBeautified, {
+			env: {
+				browser: true,
+				es6: true,
+				commonjs: true,
+				amd: true,
+				jquery: true
+			},
+			globals: {
+				TLT: true,
+				require: true,
+				define: true
+			},
+			rules: {
+				'no-implicit-globals': 2,
+				'no-undef': 2,
+				// 'no-redeclare': 2,
+				// 'no-native-reassign': 2
+			}
+		}, { filename: data.file });
+
+		self.postMessage({
+			codeBeautified: codeBeautified,
+			messages: messages
+		});
+	});
+};
+
+self.addEventListener('message', function(e) {
+	lint(e.data);
+}, false);
--- source/script.js
+++ source/script.js
@@ -5,15 +5,9 @@ var loadScript = function(file) {
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState === XMLHttpRequest.DONE) {
 				if (xhr.status === 200) {
-					resolve({
-						file: file,
-						code: xhr.responseText
-					});
+					resolve(xhr.responseText);
 				} else {
-					resolve({
-						file: file,
-						code: '/* Could not load. Status = ' + xhr.status + '*/'
-					});
+					resolve('/* Could not load. Status = ' + xhr.status + '*/');
 				}
 			}
 		};
@@ -28,114 +22,120 @@ document.addEventListener('DOMContentLoaded', function() {
 		file: 'source/get-scripts.js'
 	}, function(scriptResults) {
 		var scripts = scriptResults[0],
-			promises = [];
+			lintStack = [],
+			lintNextItemOnStack,
+			renderResults;
+
+		lintNextItemOnStack = (function() {
+			var isLinting = false;
+
+			return function() {
+				var nextScript,
+					webWorker;
+
+				if (isLinting || lintStack.length === 0) {
+					return;
+				}
+
+				isLinting = true;
+				nextScript = lintStack.shift();
+				webWorker = new Worker('eslint-web-worker.js');
+
+				webWorker.addEventListener('message', function(e) {
+					nextScript.messages = e.data.messages;
+					nextScript.codeBeautified = e.data.codeBeautified;
+					renderResults(nextScript);
+					isLinting = false;
+					lintNextItemOnStack();
+				});
+
+				webWorker.postMessage({
+					url: window.location.protocol + '//' + window.location.host,
+					file: nextScript.file,
+					code: nextScript.code
+				});
+			};
+		}());
+
+		renderResults = function(script) {
+			script.$div.textContent = '';
+
+			if (!script.file) {
+				var $inline = document.createElement('div'),
+					$inlineLink = document.createElement('a'),
+					$inlineLink2 = document.createElement('a'),
+					$inlineCode = document.createElement('code'),
+					$inlineCode2 = document.createElement('code'),
+					createClickListener = function($link, $code) {
+						$link.addEventListener('click', function(e) {
+							e.preventDefault();
+							if ($code.getAttribute('class').indexOf('hidden') === -1) {
+								$code.setAttribute('class', 'scrolling-code hidden');
+							} else {
+								$code.setAttribute('class', 'scrolling-code');
+							}
+						});
+					};
+
+				$inlineLink.textContent = 'Show inline script (original)';
+				$inlineLink2.textContent = 'Show inline script (beautified)';
+				$inlineCode.textContent = script.code;
+				$inlineCode2.textContent = script.codeBeautified;
+				$inlineLink.setAttribute('href', '#');
+				$inlineLink2.setAttribute('href', '#');
+				$inlineCode.setAttribute('class', 'scrolling-code hidden');
+				$inlineCode2.setAttribute('class', 'scrolling-code hidden');
+				createClickListener($inlineLink, $inlineCode);
+				createClickListener($inlineLink2, $inlineCode2);
+
+				script.$div.appendChild($inlineLink);
+				script.$div.appendChild($inlineCode);
+				script.$div.appendChild(document.createElement('br'));
+				script.$div.appendChild($inlineLink2);
+				script.$div.appendChild($inlineCode2);
+			}
+
+			script.messages.forEach(function(m) {
+				var $result = document.createElement('p'),
+					$summary = document.createElement('div'),
+					$code = document.createElement('code');
+
+				$summary.textContent = m.line + ':' + m.column + ' ' + m.message;
+				$code.textContent = m.source.trim();
+				$code.setAttribute('class', 'scrolling-code');
+				$result.appendChild($summary);
+				$result.appendChild($code);
+				script.$div.appendChild($result);
+			});
+
+			if (script.messages.length === 0) {
+				var $p = document.createElement('p');
+				$p.textContent = 'No errors in this file.';
+				script.$div.appendChild($p);
+			}
+		};
 
 		scripts.forEach(function(script) {
+			var $h2 = document.createElement('h2');
+			$h2.textContent = script.file || 'Inline script';
+			$content.appendChild($h2);
+			var $div = document.createElement('div');
+			$div.textContent = 'Linting...';
+			$content.appendChild($div);
+			script.$div = $div;
+
 			if (script.file) {
-				promises.push(loadScript(script.file));
+				loadScript(script.file).then(function(data) {
+					script.code = data;
+					lintStack.push(script);
+					lintNextItemOnStack();
+				});
 			} else {
-				promises.push(new Promise(function(resolve) {
-					resolve({
-						file: 'Inline script',
-						code: script.code
-					});
-				}));
+				lintStack.push(script);
 			}
 		});
 
-		Promise.all(promises).then(function(results) {
-			require(['../libs/eslint.js'], function(ESLint) {
-				var espree = require('espree');
-
-				results.forEach(function(result) {
-					var $h2 = document.createElement('h2');
-					$h2.textContent = result.file;
-					$content.appendChild($h2);
-
-					// It is hard to read the linting output for a minified file,
-					// so generate an Abstract Syntax Tree (AST), then convert
-					// the AST into a nicely formatted script with one operation per line.
-					var ast = espree.parse(result.code);
-					var newCode = escodegen.generate(ast);
-
-					if (result.file === 'Inline script') {
-						var $inline = document.createElement('div'),
-							$inlineLink = document.createElement('a'),
-							$inlineLink2 = document.createElement('a'),
-							$inlineCode = document.createElement('code'),
-							$inlineCode2 = document.createElement('code'),
-							createClickListener = function($link, $code) {
-								$link.addEventListener('click', function(e) {
-									e.preventDefault();
-									if ($code.getAttribute('class').indexOf('hidden') === -1) {
-										$code.setAttribute('class', 'scrolling-code hidden');
-									} else {
-										$code.setAttribute('class', 'scrolling-code');
-									}
-								});
-							};
-
-						$inlineLink.textContent = 'Show inline script (original)';
-						$inlineLink2.textContent = 'Show inline script (beautified)';
-						$inlineCode.textContent = result.code;
-						$inlineCode2.textContent = newCode;
-						$inlineLink.setAttribute('href', '#');
-						$inlineLink2.setAttribute('href', '#');
-						$inlineCode.setAttribute('class', 'scrolling-code hidden');
-						$inlineCode2.setAttribute('class', 'scrolling-code hidden');
-						createClickListener($inlineLink, $inlineCode);
-						createClickListener($inlineLink2, $inlineCode2);
-
-						$content.appendChild($inlineLink);
-						$content.appendChild($inlineCode);
-						$content.appendChild(document.createElement('br'));
-						$content.appendChild($inlineLink2);
-						$content.appendChild($inlineCode2);
-					}
-
-					var messages = ESLint.verify(newCode, {
-						env: {
-							browser: true,
-							es6: true,
-							commonjs: true,
-							amd: true,
-							jquery: true
-						},
-						globals: {
-							TLT: true,
-							require: true,
-							define: true
-						},
-						rules: {
-							'no-implicit-globals': 2,
-							'no-undef': 2,
-							// 'no-redeclare': 2,
-							// 'no-native-reassign': 2
-						}
-					}, { filename: result.file });
-
-					messages.forEach(function(m) {
-						var $result = document.createElement('p'),
-							$summary = document.createElement('div'),
-							$code = document.createElement('code');
-
-						$summary.textContent = m.line + ':' + m.column + ' ' + m.message;
-						$code.textContent = m.source.trim();
-						$code.setAttribute('class', 'scrolling-code');
-						$result.appendChild($summary);
-						$result.appendChild($code);
-						$content.appendChild($result);
-					});
-
-					if (messages.length === 0) {
-						var $p = document.createElement('p');
-						$p.textContent = 'No errors in this file.';
-						$content.appendChild($p);
-					}
-				});
-
-				document.getElementById('result').appendChild($content);
-			});
-		});
+		document.getElementById('result').appendChild($content);
+		lintNextItemOnStack();
 	});
 });
